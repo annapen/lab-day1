96a2fa8255226adc5d21b6415d58b3c3718f75e4
Anna Penaloza <annapenaloza@Annas-MacBook-Pro.local>
2019-02-21 20:52:37 -0500

    lab-day 1 html, css, js folders made

---
+++ main.js
@@ -0,0 +1,5 @@
+let yourpetsName = prompt('what is your pets name?')
+let favoritepetFood = prompt ('what is your pets favorite food?')
+let favoritepetActivity = prompt ('what is your pets favorite activity?')
+
+confirm(`${yourpetsName} ${favoritepetFood} is on sale for 5% off!`)
