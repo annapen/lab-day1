efada016c4706e023a0850069f73908f0f85716f
Anna Penaloza <annapenaloza@Annas-MBP.fios-router.home>
2019-03-19 12:28:27 -0400

    Adjusted the JS code and HTML to have it appear correctly below the title

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ let yourpetsName = prompt('what is your pets name?')
 let favoritepetFood = prompt ('what is your pets favorite food?')
 let favoritepetActivity = prompt ('what is your pets favorite activity?')
 
-confirm(`${yourpetsName} ${favoritepetFood} is on sale for 5% off!`)
+//confirm(`${yourpetsName} ${favoritepetFood} is on sale for 5% off!`)//
+
+let para = document.getElementById('para')
+console.log(para)
+para.innerText=`${yourpetsName} would appreciate some ${favoritepetFood} to keep ${favoritepetActivity}ing`
+//confirm(${yourpetsName} would appreciate some good food ${favoriteFood}to keep up with her ${favoritepetActicity})
+//console.log (${yourpetsName} would appreciate some good food ${favoriteFood}to keep up with her ${favoritepetActicity})
+
